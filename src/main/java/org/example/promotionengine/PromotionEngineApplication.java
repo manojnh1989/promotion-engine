d5e0b013de9bc8641d563258afd46b0f1be9c12e
Manoj Holeyannavar <mholeyannavar@onetrust.com>
2022-10-10 13:01:00 +0530

    Added base Application and Test classes.

---
+++ src/main/java/org/example/promotionengine/PromotionEngineApplication.java
@@ -0,0 +1,12 @@
+package org.example.promotionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionEngineApplication.class, args);
+    }
+}
